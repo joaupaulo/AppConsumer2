7c13c8d2e079800966320249ab08bf938b40e261
santanajps <santanajps@taginfraestrutura.com.br>
2021-11-24 22:22:23 -0300

    primeiro commit

---
+++ AppConsumer/Model/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AppConsumer.Model
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Descrption { get; set; }
+        public int Price { get; set; }
+        public int Amount { get; set; }
+        public int QuantityProducts { get; set; }
+    }
+}
---
+++ AppConsumer/QueueService/QueueConsumer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AppConsumer.DatabaseSettings;
+using AppConsumer.Model;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace AppConsumer.QueueService
+{
+    public class QueueConsumer : IHostedService
+    {
+
+        private readonly IMongoCollection<Product> _context;
+
+        public QueueConsumer(IAppConsumerDatabase settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _context = database.GetCollection<Product>(settings.ProdutosCollectionName);
+        }
+
+        public async Task CriarProduto(Product product)
+        {
+
+
+
+            await _context.InsertOneAsync(product);
+        }
+
+
+
+        public async Task<bool> VenderProduto(Product product)
+        {
+
+
+
+            var updateResult = await _context.ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+        }
+
+
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var order = System.Text.Json.JsonSerializer.Deserialize<Product>(message);
+
+
+
+                    CriarProduto(order);
+
+                };
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                
+            }
+
+
+
+
+
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
